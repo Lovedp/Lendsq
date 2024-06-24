7dc673c0481854736e91665143b9dcba9bdf0bc1
Lovedp <ifedolapoemmanuel98@gmail.com>
2024-06-24 01:27:36 +0100

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import Home from "./Pages/Users/Home";
+import Login from "./Pages/Login/Login";
+import UsersDetails from "./Pages/usersDetails/UserDetails";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import React from "react";
+import { RoutesWrapper } from "./utils/route";
+
+
+
+const App: React.FC = () => {
+  return (
+    <RoutesWrapper />
+  );
+}
+
+export default App;
